a33c266f869075e38a48c68c8c13cf32e1a53ed2
Zapt3d <aleksi.toivonen@protonmail.com>
2022-08-27 18:47:29 +0300

    Add volume slider to options

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+
+
+
     public void startGame()
     {
         SceneManager.LoadScene("GameScene");
@@ -12,7 +18,14 @@ public class MainMenuController : MonoBehaviour
 
     public void openOptions()
     {
+        settingsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
 
+    public void toMainMenu()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
     }
 
     public void quitGame()
