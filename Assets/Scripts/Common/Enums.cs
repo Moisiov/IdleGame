b1639754c45d083dadd1e49bafe1ca9024479fff
Valto Moisio <valto.moisio@pilvit.fi>
2022-08-27 22:59:26 +0300

    Add Enums script in Common.Enums namespace

---
+++ Assets/Scripts/Common/Enums.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Common.Enums
+{
+    public enum Resource
+    {
+        Alcohol,
+        Money,
+        Intoxication
+    }
+}
